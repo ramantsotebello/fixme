1784adcea7264a65ec2fbcc6b20bc69e56ddd205
Tebello Ramantso <tramants@studnet.wethinkcode.co.za>
2020-06-05 06:35:00 +0200

    Phase One

---
+++ market/src/main/java/tibz/fixMe/App.java
@@ -0,0 +1,15 @@
+package tibz.fixMe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        System.out.println( "Tibz here..." );
+    }
+}
---
+++ router/src/test/java/tibz/fixme/AppTest.java
@@ -0,0 +1,20 @@
+package tibz.fixme;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
